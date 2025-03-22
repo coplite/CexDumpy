35b1888e1ca2415613f3d9bdbed277ae9684e4b4
coplite <96002960+coplite@users.noreply.github.com>
2025-03-22 15:39:17 -0700

    Update hexdumper.c

--- hexdumper.c
+++ hexdumper.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-// gcc hexdumper.c -Wall -Werror -pedantic -O2 -flto
+// gcc hexdumper.c -Wall -Werror -pedantic -O2 -flto -o cexdumpy
 void HexDump(char* filename, unsigned char* data, uint64_t size){
     char spacedHex[192+1] = {0};
     char joinedHex[160+1] = {0};
-    short offset = 0;
+    unsigned char bufferHex[48+1] = {0};
+    unsigned char buffer[16+1] = {0};
+    unsigned short offset = 0;
     for(short i = 0; i < 8; ++i){
         offset += sprintf(spacedHex+offset,"\x1B[%dm0%x\x1B[0m ", 30+i, i);
     }
@@ -22,18 +24,35 @@ void HexDump(char* filename, unsigned char* data, uint64_t size){
     for(short i = 0; i < 8; ++i){
         offset += sprintf(joinedHex+offset,"\x1B[%dm%x\x1B[0m", 90+i, i+8);
     }
-    printf("┌──────────┬──────────────────────────────────────────────────┬──────────────────┐\n│  \x1B[94moffset\x1B[0m  │ %s │ %s │\n├──────────┼──────────────────────────────────────────────────┼──────────────────┤\n", spacedHex, joinedHex);
-    unsigned char buffer[16+1] = {0};
-    unsigned char bufferHex[16+1] = {0};
+    printf("┌──────────┬──────────────────────────────────────────────────┬──────────────────┐\n│  \x1B[94;1moffset\x1B[0m  │ %s │ %s │\n├──────────┼──────────────────────────────────────────────────┼──────────────────┤\n", spacedHex, joinedHex);
     for(uint64_t i = 0; i < size; i += 16){
-        memcpy(bufferHex, data + i, 16);
-        memcpy(buffer, bufferHex, 16);
-        for(short chr = 0; chr < sizeof(buffer); ++chr){
-            buffer[chr] = (32 <= buffer[chr] && buffer[chr] < 127) ? buffer[chr] : (short)'.';
+        memcpy(buffer, data + i, 16);
+        offset = 0;
+        for(short j = 0; j < 8; ++j){
+            offset += sprintf((char*)bufferHex+offset, "%02x ", buffer[j]);
         }
-        printf("│ \x1B[94m%08llx\x1B[0m │ %02x %02x %02x %02x %02x %02x %02x %02x  %02x %02x %02x %02x %02x %02x %02x %02x │ %-16.16s │\n", i, bufferHex[0],bufferHex[1], bufferHex[2], bufferHex[3], bufferHex[4], bufferHex[5], bufferHex[6], bufferHex[7], bufferHex[8], bufferHex[9], bufferHex[10], bufferHex[11], bufferHex[12], bufferHex[13],bufferHex[14], bufferHex[15], buffer);
+        bufferHex[offset] = ' ';
+        offset += 1;
+        for(short j = 0; j < 8; ++j){
+            offset += sprintf((char*)bufferHex+offset, "%02x ", buffer[j+8]);
+        }
+        bufferHex[48] = '\0';
+        for(short chr = 0; chr < sizeof(buffer); ++chr){
+            if(!(32 <= buffer[chr] && buffer[chr] < 127)){
+                buffer[chr] = '.';
+            }
+            if(size - i < 16 && chr >= size - i){
+                buffer[chr] = ' ';
+            }
+        }
+        for(short j = 0; j < 48; ++j){
+            if(j/3 + i >= size){            // pray that the compiler optimizes the division
+                bufferHex[j] = ' ';
+            }
+        }
+        printf("│ \x1B[94;1m%08llx\x1B[0m │ \x1b[1m%.48s\x1B[0m │ \x1b[1m%-16.16s\x1B[0m │\n", i, bufferHex, buffer);
     }
-    printf("├──────────┼──────────────────────────────────────────────────┼──────────────────┤\n│ \x1B[94m%08llx\x1B[0m │ %s │ %s │\n└──────────┴──────────────────────────────────────────────────┴──────────────────┘\n", size, spacedHex, joinedHex);
+    printf("├──────────┼──────────────────────────────────────────────────┼──────────────────┤\n│ \x1B[94;1m%08llx\x1B[0m │ %s │ %s │\n└──────────┴──────────────────────────────────────────────────┴──────────────────┘\n", size, spacedHex, joinedHex);
 }
 int main(int argc, char** argv){
     if(argc < 2){
@@ -56,11 +75,7 @@ int main(int argc, char** argv){
         perror("[-] Failed to retrieve file size\n");
         return -1;
     }
-    if(fseek(filePointer, 0, SEEK_SET)){
-        fclose(filePointer);
-        perror("[-] Unable to remodify file pointer");
-        return -1;
-    }
+    rewind(filePointer);
     unsigned char* buffer = (unsigned char*)calloc(size, sizeof(char));
     if(!buffer){
         fclose(filePointer);
@@ -77,3 +92,8 @@ int main(int argc, char** argv){
     HexDump(argv[1], buffer, size);
     free(buffer);
 }
+/*
+@   reduced 1 call to memcpy and 14 printf specifiers 
+@   but added 3 loops (2-8iters + 1-48iters) and 3 if
+@   statements and a bigger bufferHex by 32 bytes
+*/
